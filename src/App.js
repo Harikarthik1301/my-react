9e53b781f08f65a882019b544a4fe4a3043dad0d
Harikarthik1301 <135830227+Harikarthik1301@users.noreply.github.com>
2023-06-27 17:59:09 +0530

    do it

--- src/App.js
+++ src/App.js
@@ -1,69 +1,85 @@
-import React from 'react';
-import './css/App.css';
-import Head from './components/Header';
-import Footer from './components/Footer';
+import React from "react";
+import "./css/App.css";
+import Head from "./components/Header";
+import Footer from "./components/Footer";
 // import Counter from './components/Counter';
-import List from './components/List';
-import  { useState } from "react"
-import AddItems from './components/AddItems';
-import Search from './components/Search';
-
-
+import List from "./components/List";
+import { useState, useEffect } from "react";
+import AddItems from "./components/AddItems";
+import Search from "./components/Search";
 
 function App() {
-  
- const[NewItem,setNewItem]=useState('')
- const[search,setsearch]=useState('')
- const [items,setItems]= useState(JSON.parse(localStorage.getItem('anime_list')))
+  const API_URl = "http://localhost:3500/items"
+  const [NewItem, setNewItem] = useState("");
+  const [search, setsearch] = useState("");
+  //const [items,setItems]= useState(JSON.parse(localStorage.getItem('anime_list')))
+  const [items, setItems] = useState( [] );
 
- const addNewItem = (anime)=>{
-const id = items.length?items.length+1 : 1
- const adddedItem ={id,anime,checked:false}
- const NewList =[...items,adddedItem]
- console.log(NewList)
- setItems(NewList)
- localStorage.setItem("anime_list",JSON.stringify (NewList))
-}
+  useEffect(() =>{
+  const fetchitems = async () => {
+    try{
+      const response = await fetch (API_URl);
+      console.log(response);
+      const listofitems = await response.json();
+      console.log(listofitems);
+      setItems(listofitems);
+    }
+    catch(err){
+         console.log(err.stack)
+    }
+  }
+  (async () => await fetchitems ())()
+} ,[]);
 
- const handleCheck = (id) =>{
+  const addNewItem = (anime) => {
+    const id = items.length ? items.length + 1 : 1;
+    const adddedItem = { id, anime, checked: false };
+    const NewList = [...items, adddedItem];
+    console.log(NewList);
+    setItems(NewList);
+    localStorage.setItem("anime_list", JSON.stringify(NewList));
+  };
+
+  const handleCheck = (id) => {
     const animenames = items.map((item) =>
-    item.id === id ?{...item,checked:!item.checked} : item )
- setItems(animenames)
- localStorage.setItem("anime_list",JSON.stringify (animenames))
-}
+      item.id === id ? { ...item, checked: !item.checked } : item
+    );
+    setItems(animenames);
+    localStorage.setItem("anime_list", JSON.stringify(animenames));
+  };
 
-const handleDelete = (id) =>{
-const animedelete = items.filter((item) =>
-item.id !== id)
-setItems(animedelete)
-localStorage.setItem("anime_list",JSON.stringify (animedelete))
-}
-const handleAddItems = (e) =>{
-e.preventDefault()
-if(!NewItem) return;
-console.log(NewItem)
-addNewItem(NewItem)
-setNewItem('')
-}
+  const handleDelete = (id) => {
+    const animedelete = items.filter((item) => item.id !== id);
+    setItems(animedelete);
+    localStorage.setItem("anime_list", JSON.stringify(animedelete));
+  };
+  const handleAddItems = (e) => {
+    e.preventDefault();
+    if (!NewItem) return;
+    console.log(NewItem);
+    addNewItem(NewItem);
+    setNewItem("");
+  };
   return (
-    <div> 
-      <Head/>
+    <div>
+      <Head />
       {/* <Counter/> */}
       <AddItems
-      NewItem={NewItem}
-      setNewItem={setNewItem}
-      handleAddItems={handleAddItems}
+        NewItem={NewItem}
+        setNewItem={setNewItem}
+        handleAddItems={handleAddItems}
       />
-            <Search 
-      search={search}
-      setsearch={setsearch}
+      <Search search={search} setsearch={setsearch} />
+      <List
+        items={items.filter((item) =>
+          item.anime.toLowerCase().includes(search.toLowerCase())
+        )}
+        handleCheck={handleCheck}
+        handleDelete={handleDelete}
       />
-      <List items={items.filter(item => ((item.anime).toLowerCase()).includes(search.toLowerCase()))}
-      handleCheck={handleCheck}
-      handleDelete={handleDelete}/>
-      <Footer items={items}/>
+      <Footer items={items} />
     </div>
-  )
+  );
 }
 
 export default App;
--- src/components/AddItems.js
+++ src/components/AddItems.js
@@ -1,18 +1,23 @@
 import React from "react"
 import { FaPlus } from 'react-icons/fa';
+import { useRef } from "react";
+
 const AddItems = ({NewItem,setNewItem,handleAddItems}) => {
+  const InputRef = useRef()
   return (
     <div className="formadd" onClick={handleAddItems}>
     <form className='addForm'>
         <input 
         autoFocus
+        ref={InputRef}
         type="text"
         placeholder='Add Anime'
         required
         value={NewItem}
         onChange={(e)=> setNewItem (e.target.value)}
          />
-         <button type="submit">
+         <button type="submit"
+         onClick={() => InputRef.current.focus()}>
           <FaPlus/>
          </button>
     </form>
